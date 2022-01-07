0264189a79fc78b5f6e23a9343295e1c11adaea1
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-07 07:00:56 +0530

    tempconverter1.py

---
+++ Assignments/Using Loops/tempconverter1.py
@@ -0,0 +1,10 @@
+#Q3.Write a python program, which will accept temperature in celcius and convert it into fahrenheit. #HW
+#tempconverter1.py
+
+c=float(input("Enter Temperature in Celcius:"))
+
+#Formula for converting celcius into Fahrenheit and Kelvin
+f= c*(9/5)+32
+k= c+273.15
+
+print("{} C(celcius) means {} F(fahrenheit) and {} K(kelvin)".format(c,f,k))
