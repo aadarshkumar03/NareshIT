db2fd4887ae7df4efe67be48ad7fc0f224e960b8
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-07 09:28:03 +0530

    primeNum.py

---
+++ Assignments/Using Loops/primeNum.py
@@ -0,0 +1,17 @@
+'''Q5.Write a python program which will decide whether the given number is prime or not. #CW & HW
+primeNum.py'''
+
+n=int(input("Enter a number:"))
+if(n<=1):
+	print("{} is invalid input".format(n))
+else:
+	result=True
+	for i in range(2,n):
+		if(n%i==0):
+			result=False
+			break
+	
+	if(result):
+		print("{} is PRIME Number:".format(n))
+	else:
+		print("{} is NOT PRIME Number:".format(n))
