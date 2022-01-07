6a9573919541e67982f8cd9afbae8adf874c032d
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-07 09:11:22 +0530

    naturalSqSum.py

---
+++ Assignments/Using Loops/naturalSqSum.py
@@ -0,0 +1,16 @@
+'''Q2.Write a python program which will find sum of squares of first n natural numbers. #CW
+naturalSqSum.py'''
+
+n=int(input("Enter a Number : "))
+if n<=0:
+    print("Invalid Number...")
+else:
+    i=1
+    s=0
+    while i<=n:
+        print(i**2,end=" ")
+        s=s+i**2
+        i+=1
+    else:
+        print("\nThe sum of Square of First {} Natural Numbers={}".format(n,s))
+print("Program Executed!!!")
