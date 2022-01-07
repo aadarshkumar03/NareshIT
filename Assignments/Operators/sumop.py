5f0b546aae00d39160f9bf5c8d4da8b81b8bc94b
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-07 07:03:56 +0530

    sumop.py

--- Assignments/Operators/sumop.py
+++ Assignments/Operators/sumop.py
@@ -1,4 +1,6 @@
 #Q1.Write a python program, which will accepts two numerical values and find its sum. #CW
+#sumop.py
+
 a=int(input("Enter First Number : "))
 b=int(input("Enter Second Number : "))
 c=a+b
