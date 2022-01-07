d3b12ca1c09f5038a81014189b8d321bf85c08e0
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-07 07:34:45 +0530

    evenodd.py

---
+++ Assignments/Using Loops/evenodd.py
@@ -0,0 +1,10 @@
+# Q3.Write a python program, which will accept any numerical value and decide whether it is even or odd. #HW
+# Using Simple if Statement
+# evenodd.py
+
+a=int(input("Enter Numerical value :"))
+if a%2==0:
+    print("{} is even Number".format(a))
+if a%2!=0:
+    print("{} is odd Numer".format(a))
+print("Program Executed!!")
