93bdbdf6b9298717a87702230745416468ebff71
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-07 07:32:35 +0530

    PosNeg.py

---
+++ Assignments/Using Loops/PosNeg.py
@@ -0,0 +1,12 @@
+# Q1.Write a python program, which will accept a number and decide whether it is positive, negative or zero. #CW
+# Using Simple if Statement
+# PosNeg.py
+
+a=int(input("Enter Any Integer Number :"))
+if a==0:
+    print("{} is ZERO".format(a))
+if a>0:
+    print("{} is Positive Number".format(a))
+if a<0:
+    print("{} is Negative Number".format(a))
+print("Program Executed!!!)
