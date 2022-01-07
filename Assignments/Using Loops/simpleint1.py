40f33fda50c7d29643bafe1c7d90e7f579f20756
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-07 06:58:57 +0530

    simpleint1.py

---
+++ Assignments/Using Loops/simpleint1.py
@@ -0,0 +1,23 @@
+#Q2.Write a python program, which will calculate simple interest and total amount of pay. #CW
+#simpleint1.py
+
+p=float(input("Enter Principle Amount:"))
+t=float(input("Enter Time:"))
+r=float(input("Enter Rate of Interest:"))
+
+#calcuate si
+si=(p*t*r)/100
+
+#calculate total amount to pay
+totamt=p+si
+
+print("-"*50)
+print("\tSimple Intrest results")
+print("-"*50)
+print("\tPrinciple Amount={}".format(p))
+print("\tTime={}".format(t))
+print("\tRate of Interest={}".format(r))
+print("-"*50)
+print("\tSimple Interest on Princple={}".format(si))
+print("\tTotal Amount={}".format(totamt))
+print("-"*50)
