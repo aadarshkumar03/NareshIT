0b7805d221bd4c894de8a54d5876cc711538b3b7
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-07 07:16:54 +0530

    basepow.py

---
+++ Assignments/Operators/basepow.py
@@ -0,0 +1,9 @@
+# Q8.Write a python program, which will calculate a^m, here a is called base and m is called power. #HW
+# basepow.py
+
+# Taking Inputs
+a=float(input("Enter Base Value:"))
+m=int(input("Enter Power Value:"))
+
+# Calculate a^m
+print("{} to the power of {} = {}".format(a,m,a**m))
