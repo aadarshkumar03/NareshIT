7dfc6a7c205414eeef8066b763f7a965b6495fe0
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-07 06:54:20 +0530

    sum1.py

---
+++ Assignments/Using Loops/sum1.py
@@ -0,0 +1,5 @@
+#Q.Write a python program, which will accepts two numerical values and find its sum. #CW
+a=int(input("Enter First Number : "))
+b=int(input("Enter Second Number : "))
+c=a+b
+print("The sum ({},{}) = {}".format(a,b,a+b))
