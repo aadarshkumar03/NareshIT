5fdcfc1b306101cf9208e2491acbbc8729ae7a77
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-07 09:17:13 +0530

    palinNum.py

---
+++ Assignments/Using Loops/palinNum.py
@@ -0,0 +1,19 @@
+'''Q6.Write a python program which will accept numerical integer value and check whether the number is palindrome or not. #CW
+palinNum.py'''
+
+n=int(input("Enter a Number : "))
+if n<0:
+    print("Invalid Number.")
+else:
+    tn=n
+    r=0
+    while n>0:
+        d=n%10
+        r=r*10+d
+        n=n//10
+    else:
+        if r==tn:
+            print("{} is Palindrome".format(tn))
+        else:
+            print("{} Not Palindrome".format(tn))
+print("Program Executed!!!")
