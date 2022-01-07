94127670f864470653b806e0771a157cf908c277
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-07 09:14:43 +0530

    naturalAllSum.py

---
+++ Assignments/Using Loops/naturalAllSum.py
@@ -0,0 +1,20 @@
+'''Q4.Write a python program which will calculate sum of first n natural numbers and squares and cubes of first n natural numbers. #CW
+naturalAllSum.py'''
+
+n=int(input("Enter a Number : "))
+if n<=0:
+    print("Invalid Number")
+else:
+    i=1
+    s1,s2,s3=0,0,0
+    while i<=n:
+        print("\t{}\t{}\t{}".format(i,i**2,i**3))
+        s1=s1+i
+        s2=s2+i**2
+        s3=s3+i**3
+        i+=1
+    else:
+        print("\nThe sum of first {} Natural Numbers={}".format(n,s1))
+        print("The sum of Square of first {} Natural Numbers={}".format(n,s2))
+        print("The sum of Cubes of First {} Natural Nubers={}".format(n,s3))
+print("Program Exucted!!!")
